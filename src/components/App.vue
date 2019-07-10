91b38b45089fa01e262b03ff088abd5ffa6b131e
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2019-07-10 19:19:32 +0200

    added example content

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <icon icon-id="arrow-right" />
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon.vue'
+
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style lang="scss" src="@/assets/styles/global.scss" />
---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <component class="icon" :is="IconSVG" />
+</template>
+
+<script>
+export default {
+  props: {
+    iconId: {
+      required: true,
+      type: String
+    }
+  },
+  computed: {
+    IconSVG() {
+      return () => import(`@/assets/icons/${this.iconId}.svg`)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.icon {
+  width: 20px;
+  height: 20px;
+}
+</style>
