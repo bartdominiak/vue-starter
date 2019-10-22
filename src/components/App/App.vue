9012e9f164f43a32e2e928d8a16688cb2d68e9d5
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2019-10-22 12:35:50 +0200

    moved components to seperate folder, make styles extendable, added AppLink

---
+++ src/components/App/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <MainLayout>
+    <AppLink
+      href="http://github.com/bartdominiak"
+      target="_blank"
+    >
+      <AppIcon icon-id="github" />
+    </AppLink>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from '@/templates/MainLayout.vue';
+import AppIcon from '@/components/AppIcon/AppIcon.vue';
+import AppLink from '@/components/AppButton/AppLink.vue';
+
+export default {
+  components: {
+    MainLayout,
+    AppIcon,
+    AppLink
+  }
+};
+</script>
+
+<style lang="scss" src="@/assets/styles/global.scss" />
