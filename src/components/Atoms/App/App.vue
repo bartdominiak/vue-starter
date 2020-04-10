2cd9adbf24dfa4463a4fe2a5983c5232c336dc7d
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2020-04-10 12:46:22 +0200

    added components and assets

---
+++ src/components/Atoms/App/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <MainLayout>
+    <base-button
+      tag="a"
+      href="http://github.com/bartdominiak"
+      target="_blank"
+    >
+      <GithubIcon class="button__icon" />
+    </base-button>
+  </MainLayout>
+</template>
+
+<script>
+import MainLayout from '@/components/Templates/MainLayout.vue'
+import BaseButton from '@/components/Atoms/Button/Button.vue'
+import GithubIcon from '@/assets/icons/github.svg'
+
+export default {
+  components: {
+    MainLayout,
+    GithubIcon,
+    BaseButton
+  }
+}
+</script>
+
+<style lang="scss" src="@/assets/styles/global.scss" />
---
+++ src/components/Atoms/Button/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <component
+    :is="tag"
+    :type="tag === 'button' ? type : false"
+    class="button"
+  >
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+    * HTML tag type
+    */
+    tag: {
+      type: String,
+      default: 'button'
+    },
+    /**
+    * HTML type
+    */
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./_button.scss" />
